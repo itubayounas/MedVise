2161d0ef8694c26060b11175b88c1bc32550c953
Tuba Younas <tubayounas205@gmail.com>
2026-04-29 20:45:27 +0500

    added the deployed backened url

--- healthCare_Frontend/src/api/axios.js
+++ healthCare_Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://med-vise-backened.vercel.app/api" || "http://localhost:5000/api",
 });
 
 api.interceptors.request.use((config) => {
